b5f40b3f0b3d200193054004044a181bf86b2384
zhwan2845 <johaeun0802@naver.com>
2025-05-26 17:04:52 +0900

    [Fix] 가게 리뷰 등록 에러 헨들러 수정

--- src/main/java/umc/springboot/service/ReviewService/ReviewCommandServiceImpl.java
+++ src/main/java/umc/springboot/service/ReviewService/ReviewCommandServiceImpl.java
@@ -5,10 +5,13 @@ import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import umc.springboot.apiPayload.code.status.ErrorStatus;
+import umc.springboot.apiPayload.exception.handler.StoreHandler;
 import umc.springboot.domain.Member;
 import umc.springboot.domain.Review;
 import umc.springboot.domain.Store;
 import umc.springboot.repository.ReviewRepository.ReviewRepository;
+import umc.springboot.repository.StoreRepository.StoreRepository;
 
 import java.time.LocalDateTime;
 
@@ -19,12 +22,15 @@ public class ReviewCommandServiceImpl implements ReviewCommandService {
 
     private final EntityManager em;
     private final ReviewRepository reviewRepository;
+    private final StoreRepository storeRepository;
 
     @Override
     @Transactional
     public Review saveReview(Long memberId, Long storeId, String title, String body, Float score) {
         Member member = em.getReference(Member.class, memberId);
-        Store store = em.getReference(Store.class, storeId);
+
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new StoreHandler(ErrorStatus.STORE_NOT_FOUND));
 
         Review review = Review.builder()
                 .member(member)
